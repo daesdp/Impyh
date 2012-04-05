b3fdd75bb1d56323365ab5ee8a7d8556e1603a10
Daes <daes.dp@gmail.com>
2012-04-05 02:52:15 -0300

    update v0.5

--- impyh/usr/share/impyh/impyh.py
+++ impyh/usr/share/impyh/impyh.py
@@ -26,7 +26,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk,Gdk , GdkPixbuf
+from gi.repository import Gtk, GdkPixbuf
 from gi.repository.GdkPixbuf import Pixbuf
 import os
 
